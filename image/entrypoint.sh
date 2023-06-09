5103eb6b168aec9c5894c84ba978d3cefd784813
Justin Pflueger <justin.pflueger@fermyon.com>
2023-06-09 17:02:12 -0600

    make config query configurable

--- image/entrypoint.sh
+++ image/entrypoint.sh
@@ -7,8 +7,10 @@ set -euo
 ##
 HOST_CONTAINERD_CONFIG="${HOST_CONTAINERD_CONFIG:-/host/etc/containerd/config.toml}"
 HOST_BIN="${HOST_BIN:-/host/bin}"
-RUNTIME_TYPE="io.containerd.spin.v1"
-RUNTIME_HANDLE="spin"
+
+RUNTIME_CONFIG_TYPE="${RUNTIME_CONFIG_TYPE:-io.containerd.spin.v1}"
+RUNTIME_CONFIG_HANDLE="${RUNTIME_CONFIG_HANDLE:-spin}"
+RUNTIME_CONFIG_TABLE="plugins.\"io.containerd.grpc.v1.cri\".containerd.runtimes.${RUNTIME_HANDLE}"
 
 ##
 # helper functions
@@ -18,21 +20,21 @@ run_as_host() {
 }
 
 get_runtime_type() {
-  toml get -r "${1}" 'plugins."io.containerd.grpc.v1.cri".containerd.runtimes.spin.runtime_type'
+  toml get -r "${1}" "${RUNTIME_CONFIG_TABLE}.runtime_type"
 }
 
 set_runtime_type() {
-  echo "adding spin runtime '${RUNTIME_TYPE}' to ${HOST_CONTAINERD_CONFIG}"
+  echo "adding spin runtime '${RUNTIME_CONFIG_TYPE}' to ${HOST_CONTAINERD_CONFIG}"
   tmpfile=$(mktemp)
-  toml set "${HOST_CONTAINERD_CONFIG}" 'plugins."io.containerd.grpc.v1.cri".containerd.runtimes.spin.runtime_type' "${RUNTIME_TYPE}" > "${tmpfile}"
+  toml set "${HOST_CONTAINERD_CONFIG}" "${RUNTIME_CONFIG_TABLE}.runtime_type" "${RUNTIME_CONFIG_TYPE}" > "${tmpfile}"
 
   # ensure the runtime_type was set
-  if [ "$(get_runtime_type "${tmpfile}")" = "${RUNTIME_TYPE}" ]; then
+  if [ "$(get_runtime_type "${tmpfile}")" = "${RUNTIME_CONFIG_TYPE}" ]; then
     # overwrite the containerd config with the temp file
     mv "${tmpfile}" "${HOST_CONTAINERD_CONFIG}"
     echo "committed changes to containerd config"
   else
-    echo "failed to set runtime_type to ${RUNTIME_TYPE}"
+    echo "failed to set runtime_type to ${RUNTIME_CONFIG_TYPE}"
     exit 1
   fi
 }
@@ -59,8 +61,8 @@ echo "copying the shim to the node's bin directory '${HOST_BIN}'"
 cp "./containerd-shim-spin-v1" "${HOST_BIN}"
 
 # check if the shim is already in the containerd config
-if [ "$(get_runtime_type "${HOST_CONTAINERD_CONFIG}")" = "${RUNTIME_TYPE}" ]; then
-  echo "runtime_type is already set to ${RUNTIME_TYPE}"
+if [ "$(get_runtime_type "${HOST_CONTAINERD_CONFIG}")" = "${RUNTIME_CONFIG_TYPE}" ]; then
+  echo "runtime_type is already set to ${RUNTIME_CONFIG_TYPE}"
 else
   set_runtime_type
 fi
